c1b4f785e20b330eb3f8062bcba3dbd96191cc6d
Anastasiya Kakura <A.Kakura@ibm.com>
2021-03-01 12:38:01 +0300

    task3

--- src/components/content/card/Card.js
+++ src/components/content/card/Card.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './Card.scss';
 import PropTypes from 'prop-types';
+import { FiCheckCircle, FiXCircle, FiEdit } from 'react-icons/fi';
 
 class Card extends React.Component {
   constructor(props) {
     super(props);
+    const { title, decr } = this.props;
     this.state = {
       selectedCard: false,
+      isEdited: false,
+      title,
+      decr,
     };
+
+    this.title = React.createRef();
+    this.decr = React.createRef();
   }
 
   handleCheckboxClick = () => {
@@ -15,29 +23,82 @@ class Card extends React.Component {
     this.setState({ selectedCard: !selectedCard });
   }
 
+  onEdit = () => {
+    this.setState({ isEdited: true, selectedCard: false });
+  }
+
+  onCancel = () => {
+    this.setState({ isEdited: false, selectedCard: false });
+  }
+
+  onSubmit = () => {
+    this.setState({
+      isEdited: false,
+      selectedCard: false,
+      title: this.title.current.value,
+      decr: this.decr.current.value,
+    });
+  }
+
   render() {
-    const { name, decr } = this.props;
-    const { selectedCard } = this.state;
+    const { selectedCard, isEdited, title, decr } = this.state;
     const className = selectedCard ? 'card selected' : 'card';
+
+    if (isEdited) {
+      return (
+        <div className={className}>
+          <div className="title row">
+            <div className="col-8">
+              <textarea
+                ref={this.title}
+                className="form-control textarea-title"
+                type="text"
+                defaultValue={title}
+              />
+            </div>
+            <div className="col-1 actionButton">
+              <FiXCircle className="fiXButton" onClick={this.onCancel} />
+            </div>
+            <div className="col-1 actionButton">
+              <FiCheckCircle
+                className="fiXButton"
+                onClick={this.onSubmit}
+              />
+            </div>
+          </div>
+          <div className="card-body">
+            <textarea
+              ref={this.decr}
+              className="form-control textarea"
+              type="text"
+              defaultValue={decr}
+            />
+          </div>
+        </div>
+      );
+    }
 
     return (
       <div className={className}>
         <div className="title row">
-          <div className="col-10">
-            {name}
+          <div className="col-8 title-text" title={title}>
+            {title}
           </div>
           <div className="col-1">
             <input type="checkbox" onClick={this.handleCheckboxClick} />
           </div>
+          <div className="col-1">
+            <FiEdit onClick={this.onEdit} />
+          </div>
         </div>
-        <div className="decr"> {decr} </div>
+        <div className="card-body decr"> {decr} </div>
       </div>
     );
   }
 }
 
 Card.propTypes = {
-  name: PropTypes.string,
+  title: PropTypes.string,
   decr: PropTypes.string,
 };
 
