a478ad524a55a3d198d114ca709ce9a04741c3df
Anastasiya Kakura <A.Kakura@ibm.com>
2021-03-04 11:11:30 +0300

    task4

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -2,10 +2,84 @@ import React from 'react';
 import Card from './card';
 import './Content.scss';
 
-const Content = () => (
-  <div className="content">
-    <Card title="First Card" decr="Something" />
-  </div>
-);
+class Content extends React.Component {
+  constructor(props) {
+    super(props);
+    const cards = [
+      {
+        id: 1,
+        title: 'First Card',
+        descr: 'Descriptopn',
+      },
+      {
+        id: 2,
+        title: 'Second Card',
+        descr: 'Descriptopn',
+      },
+      {
+        id: 3,
+        title: 'Third Card',
+        descr: 'Descriptopn',
+      },
+      {
+        id: 4,
+        title: 'Fourth Card',
+        descr: 'Descriptopn',
+      },
+      {
+        id: 5,
+        title: 'Fifth Card',
+        descr: 'Descriptopn',
+      },
+      {
+        id: 6,
+        title: 'Sixth Card',
+        descr: 'Descriptopn',
+      },
+      {
+        id: 7,
+        title: 'Seventh Card',
+        descr: 'Descriptopn',
+      },
+      {
+        id: 8,
+        title: 'Eighth Card',
+        descr: 'Descriptopn',
+      },
+      {
+        id: 9,
+        title: 'Ninth Card',
+        descr: 'Descriptopn',
+      }
+    ]
+    this.state = {
+      viewOnly: false,
+      cards,
+    };
+  }
+
+  onViewOnlyChanged = () => {
+    const { viewOnly } = this.state;
+    this.setState({ viewOnly: !viewOnly });
+  }
+
+  render() {
+    const { viewOnly, cards } = this.state;
+
+    return (
+      <div className="content">
+        <div className="row viewOnlyCheckbox">
+          <input id="viewOnlyCheckbox" className="form-check-input" value={viewOnly} type="checkbox" onClick={this.onViewOnlyChanged} />
+          <label className="form-check-label" htmlFor="viewOnlyCheckbox">View only</label>
+        </div>
+        <div className="row">
+          {cards.map(card => (<Card title={card.title} descr={card.descr} key={card.id} viewOnly={viewOnly} />))}
+        </div>
+
+      </div>
+    );
+  }
+
+}
 
 export default Content;
--- src/components/content/card/Card.js
+++ src/components/content/card/Card.js
@@ -6,17 +6,29 @@ import { FiCheckCircle, FiXCircle, FiEdit } from 'react-icons/fi';
 class Card extends React.Component {
   constructor(props) {
     super(props);
-    const { title, decr } = this.props;
+    const { title, descr } = this.props;
     this.state = {
       selectedCard: false,
       isEdited: false,
       title,
-      decr,
+      descr,
     };
 
     this.title = React.createRef();
-    this.decr = React.createRef();
+    this.descr = React.createRef();
   }
+  
+  componentDidUpdate(nextProps) {
+    const { viewOnly } = this.props
+    if (nextProps.viewOnly !== viewOnly) {
+      this.stopEdit();
+    }
+  }
+
+  stopEdit = () => {
+    this.setState({ isEdited: false });
+  }
+
 
   handleCheckboxClick = () => {
     const { selectedCard } = this.state;
@@ -36,66 +48,79 @@ class Card extends React.Component {
       isEdited: false,
       selectedCard: false,
       title: this.title.current.value,
-      decr: this.decr.current.value,
+      descr: this.descr.current.value,
     });
   };
 
+  renderEditable = () => {
+    const { title, descr } = this.state;
+    return (
+      <>
+        <div className="title row">
+          <div className="col-8">
+            <textarea
+              ref={this.title}
+              className="form-control textarea-title"
+              type="text"
+              defaultValue={title}
+            />
+          </div>
+          <div className="col-1 actionButton">
+            <FiXCircle className="fiXButton" onClick={this.onCancel} />
+          </div>
+          <div className="col-1 actionButton">
+            <FiCheckCircle className="fiXButton" onClick={this.onSubmit} />
+          </div>
+        </div>
+        <div className="card-body">
+          <textarea
+            ref={this.descr}
+            className="form-control textarea"
+            type="text"
+            defaultValue={descr}
+          />
+        </div>
+      </>
+    );
+  };
+
+  renderNotEditable = () => {
+    const { title, descr } = this.state;
+    const { viewOnly } = this.props;
+    return (
+      <>
+        <div className="title row">
+          <div className="col-8 title-text" title={title}>
+            {title}
+          </div>
+          <div className="col-1">
+            <input type="checkbox" onClick={this.handleCheckboxClick} />
+          </div>
+          <div className="col-1 edit-button">
+          {!viewOnly && <FiEdit onClick={this.onEdit} />}
+          </div>
+        </div>
+        <div className="card-body descr"> {descr} </div>
+      </>
+    );
+  };
+
   render() {
-    const { selectedCard, isEdited, title, decr } = this.state;
+    const { selectedCard, isEdited } = this.state;
     const className = selectedCard ? 'card selected' : 'card';
 
     return (
       <div className={className}>
-        {isEdited ? (
-          <div className="title row">
-            <div className="col-9">
-              <textarea
-                ref={this.title}
-                className="form-control textarea-title"
-                type="text"
-                defaultValue={title}
-              />
-            </div>
-            <div className="col-1 actionButton">
-              <FiXCircle className="fiXButton" onClick={this.onCancel} />
-            </div>
-            <div className="col-1 actionButton">
-              <FiCheckCircle className="fiXButton" onClick={this.onSubmit} />
-            </div>
-          </div>
-        ) : (
-          <div className="title row">
-            <div className="col-8 title-text" title={title}>
-              {title}
-            </div>
-            <div className="col-1">
-              <input type="checkbox" onClick={this.handleCheckboxClick} />
-            </div>
-            <div className="col-1 edit-button">
-              <FiEdit onClick={this.onEdit} />
-            </div>
-          </div>
-        )}
-        {isEdited ? (
-          <div className="card-body">
-            <textarea
-              ref={this.decr}
-              className="form-control textarea"
-              type="text"
-              defaultValue={decr}
-            />
-          </div>
-        ) : (
-          <div className="card-body decr"> {decr} </div>
-        )}
+        {isEdited ? this.renderEditable() : this.renderNotEditable()}
       </div>
     );
   }
 }
 
 Card.propTypes = {
+  viewOnly: PropTypes.bool,
   title: PropTypes.string,
-  decr: PropTypes.string,
+  descr: PropTypes.string,
 };
 
 export default Card;
