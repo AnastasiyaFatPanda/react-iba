5b54658761bbdc2332f509edfb13954f3b0abbb9
Anastasiya Kakura <A.Kakura@ibm.com>
2021-05-17 16:13:19 +0300

    task12

---
+++ src/components/login/input/Input.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Input.scss';
+import PropTypes from 'prop-types';
+
+const Input = props => {
+    const { placeholder, type, value, onChange, onBlur, valid, touched } = props;
+    const error = !valid && touched;
+    return (
+        <div className="form-group">
+            <label htmlFor={type}>
+                {placeholder}
+            </label>
+            <input
+                type={type}
+                className={error ? 'form-control invalidInput' : 'form-control'}
+                placeholder={placeholder}
+                autoComplete="on"
+                value={value}
+                onBlur={onBlur}
+                onChange={onChange} />
+            {error && <div className="errorMessage">Type correct {type}</div>}
+        </div>
+    );
+};
+
+Input.propTypes = {
+    placeholder: PropTypes.string,
+    type: PropTypes.string,
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+    onBlur: PropTypes.func,
+    valid: PropTypes.bool,
+    touched: PropTypes.bool,
+};
+
+export default Input;
